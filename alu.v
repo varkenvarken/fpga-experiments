9cf191816bb8fb1415de3c6c543ed52c294130ca
varkenvarken <michel@michelanders.nl>
2019-11-18 21:42:44 +0100

    implemented SUB/SBC

--- alu.v
+++ alu.v
@@ -8,10 +8,13 @@ module alu(
 	output zero
 	);
 
-	wire [8:0] add,adc,result;
+	// TODO: overflow/underflow
+	wire [8:0] add,adc,sub,sbc,result;
 	assign add = {0, a} + {0, b};
 	assign adc = add + { 8'd0, carry};
-	assign result = op[3:1] ? {0, a} : ( op[0] ? adc : add ); // all undefined opcodes simply pass A thru
+	assign sub = {0, a} - {0, b};
+	assign sbc = add - { 8'd0, carry};
+	assign result = op[3:2] ? {0, a} : op[1]? ( op[0] ? sbc : sub ): ( op[0] ? adc : add ); // all undefined opcodes simply pass A thru
 	assign carry_out = result[8];
 	assign c = result[7:0];
 	assign zero = (c == 0);
