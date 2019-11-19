b62e2a48514b444cd79499b62245e5efa0ae15d8
varkenvarken <michel@michelanders.nl>
2019-11-19 13:55:34 +0100

    implemented bitwise logic

--- alu.v
+++ alu.v
@@ -10,11 +10,20 @@ module alu(
 
 	// TODO: overflow/underflow
 	wire [8:0] add,adc,sub,sbc,result;
+	wire [7:0] b_and,b_or,b_xor,b_not;
 	assign add = {0, a} + {0, b};
 	assign adc = add + { 8'd0, carry};
 	assign sub = {0, a} - {0, b};
 	assign sbc = add - { 8'd0, carry};
-	assign result = op[3:2] ? {0, a} : op[1]? ( op[0] ? sbc : sub ): ( op[0] ? adc : add ); // all undefined opcodes simply pass A thru
+	assign b_and = {0, a & b};
+	assign b_or  = {0, a | b};
+	assign b_xor = {0, a ^ b};
+	assign b_not = {0,~a    };
+	assign result = op[3:2] == 0 ? // addition/subtraction
+									( op[1] ? ( op[0] ? sbc : sub ): ( op[0] ? adc : add ) )
+				:	op[3:2] == 1 ? // bitwise logic
+									( op[1] ? ( op[0] ? b_xor : b_not ): ( op[0] ? b_and : b_or ) )
+				: {0, a} ; // all undefined opcodes simply pass A thru
 	assign carry_out = result[8];
 	assign c = result[7:0];
 	assign zero = (c == 0);
