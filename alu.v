fa15a18682ac442a33a021875a8ce9b143cd03d5
varkenvarken <michel@michelanders.nl>
2019-11-27 21:08:42 +0100

    fixed wrong subc and removed tstb and neg to save space

--- alu.v
+++ alu.v
@@ -11,12 +11,13 @@ module alu(
 
 	// TODO: overflow/underflow
 	wire [8:0] add,adc,sub,sbc,extend,min_a,result, shiftl, shiftr, shiftlc, shiftrc;
+	reg [8:0] result;
 	wire [7:0] b_and,b_or,b_xor,b_not, cmp;
 
 	assign add = {0, a} + {0, b};
 	assign adc = add + { 8'd0, carry};
 	assign sub = {0, a} - {0, b};
-	assign sbc = add - { 8'd0, carry};
+	assign sbc = sub - { 8'd0, carry};
 	assign b_and = {0, a & b};
 	assign b_or  = {0, a | b};
 	assign b_xor = {0, a ^ b};
@@ -29,14 +30,36 @@ module alu(
 	assign shiftlc = {a[7:0],carry};
 	assign shiftrc = {a[0],carry,a[7:1]};
 
-	assign result = op[3:2] == 0 ? // addition/subtraction
-									( op[1] ? ( op[0] ? sbc : sub ): ( op[0] ? adc : add ) )
-				:(	op[3:2] == 1 ? // bitwise logic
-									( op[1] ? ( op[0] ? b_xor : b_not ): ( op[0] ? b_and : b_or ) )
-				:(	op[3:2] == 2 ? // test, invert, compare
-									( op[1] ? ( op[0] ? cmp : min_a ): ( op[0] ? {0, b} : {0, a} ) )
-								   // shifts
-				: 					( op[1] ? ( op[0] ? shiftrc : shiftlc ): ( op[0] ? shiftr : shiftl ) ) ) );
+//	assign result = op[3:2] == 0 ? // addition/subtraction
+//									( op[1] ? ( op[0] ? sbc : sub ): ( op[0] ? adc : add ) )
+//				:(	op[3:2] == 1 ? // bitwise logic
+//									( op[1] ? ( op[0] ? b_xor : b_not ): ( op[0] ? b_and : b_or ) )
+//				:(	op[3:2] == 2 ? // test, invert, compare
+//									( op[1] ? ( op[0] ? cmp : min_a ): ( op[0] ? {0, b} : {0, a} ) )
+//								   // shifts
+//				: 					( op[1] ? ( op[0] ? shiftrc : shiftlc ): ( op[0] ? shiftr : shiftl ) ) ) );
+
+	always @(*) begin
+	case (op)
+		0: result <= add;
+		1: result <= adc;
+		2: result <= sub;
+		3: result <= sbc;
+		4: result <= b_or;
+		5: result <= b_and;
+		6: result <= b_not;
+		7: result <= b_xor;
+		8: result <= {0, a};
+		9, //: result <= {0, b};
+		10, //: result <= min_a;
+		11: result <= cmp;
+		12: result <= shiftl;
+		13: result <= shiftl;
+		14: result <= shiftlc;
+		15: result <= shiftrc;
+	endcase
+	end
+
 	assign c = result[7:0];
 	assign carry_out = result[8];
 	assign zero = (c == 0);
